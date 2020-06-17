1bf3087ad7030fe9dbc4b156a2b1e0a1fbd68c62
Andy Rea <email@andrewrea.co.uk>
2020-06-17 13:15:25 +0100

    Fixed the tracking code

--- static/js/ga.js
+++ static/js/ga.js
@@ -1,3 +1,3 @@
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-ga("create", "UA-2048754-7", "auto");
+ga("create", "UA-2048754-6", "auto");
 ga("send", "pageview");
