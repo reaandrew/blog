296bd28b1e13197e2a0632483a5ecf6b5eaa7e28
Andy Rea <email@andrewrea.co.uk>
2022-03-16 12:27:13 +0000

    Add Google Analytics

--- static/js/ga.js
+++ static/js/ga.js
@@ -1,3 +1,13 @@
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 ga("create", "UA-2048754-6", "auto");
 ga("send", "pageview");
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1GGLJTLKRT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-1GGLJTLKRT');
+</script>
